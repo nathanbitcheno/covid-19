501ee9a826298fc957dec6fac898e5cfde1e7f17
Nathan Bitcheno <Nathan@pdnz.co.nz>
2020-06-02 11:17:50 +1200

    vue analytics

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueAnalytics from 'vue-analytics'
 
 Vue.config.productionTip = false
+
+// Congfigure Vue Analytics (Google analytics)
+Vue.use(VueAnalytics, {
+  id: 'UA-168225551-1',
+  router
+})
 
 const pageTitle = 'Simple COVID-19 Tracker'
 document.title = pageTitle
